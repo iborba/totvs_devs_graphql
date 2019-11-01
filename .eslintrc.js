c76f4a4446e440de6f108fd0746d9ea7aa48c5bb
Israel Borba <israel.borba@totvs.com.br>
2019-11-01 18:47:56 -0300

    Second commit, none bugs, but not working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,19 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  env: {
+    es6: true,
+    node: true
+  },
+  plugins: ['@typescript-eslint'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    'standard'
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,39 @@
-const calc = ((b: number) => {
-  const soma = ((a: number) => {
-    return a + b;
-  });
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+const app = express()
+import DataLoader from 'dataloader'
+import util from 'util'
+const schema = require('./schema')
+const parseXML = util.promisify(require('xml2js').parseString)()
 
-  return soma;
-});
 
-console.log(calc(5)(4))
+const fetchAuthor = id => {
+  fetch(`https://www.goodreads.com/author/show.xml?id=${id}&key=Rz607gLuIzr1a55wEbl3w `)
+    .then(response => {
+      console.log('dmkasjdsk')
+      response.text()
+    })
+    .then(parseXML)
+}
+
+const fetchBook = id => {
+  fetch(`https://www.goodreads.com/book/show/${id}.xml?key=Rz607gLuIzr1a55wEbl3w `)
+    .then(response => response.text())
+    .then(parseXML)
+}
+
+app.use('/graphql', graphqlHTTP(req => {
+  const loadAuthor = new DataLoader(k => Promise.all(k.map(fetchAuthor)))
+  const loadBook = new DataLoader(k => Promise.all(k.map(fetchBook)))
+
+  return {
+    schema,
+    context: {
+      loadAuthor,
+      loadBook
+    },
+    graphiql: true
+  }
+}))
+
+app.listen(3333, (_ => { console.log("Listening at 3333") }))
