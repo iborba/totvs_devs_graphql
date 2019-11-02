abeba4d8eae5e4ba36212e5f43a2e7d5b751fd85
Israel Borba <israel.borba@totvs.com.br>
2019-11-02 16:45:54 -0300

    Schema pronto e projeto rodando

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,39 @@
-const a: number = 91
+import { ApolloServer, gql } from 'apollo-server'
+import { makeExecutableSchema } from 'graphql-tools'
 
-console.log('🚀 ok' + a)
+const port = 3000
+
+const definition = {
+  typeDefs: gql`
+      type Query {
+        _empty: String
+      }
+      
+      type Mutation { 
+        _empty: String
+      }
+
+      type Subscription { 
+        _empty: String
+      }
+  `,
+  resolvers: {}
+}
+
+const schema = makeExecutableSchema(definition)
+
+const serverConfig = {
+  schema,
+  playGround: {
+    settings: {
+      'editor.theme': 'dark',
+      'editor.cursorShape': 'line'
+    }
+  }
+}
+
+const server = new ApolloServer(serverConfig)
+
+server.listen(port).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
