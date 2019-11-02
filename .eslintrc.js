2dc1b1d4ab1668db1c08743035eaa86f10fa13bd
Israel Borba <israel.borba@totvs.com.br>
2019-11-02 14:47:28 -0300

    Clean project + eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
   env: {
+    browser: true,
     es6: true,
     node: true
   },
-  plugins: ['@typescript-eslint'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'standard'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  rules: {}
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
 }
