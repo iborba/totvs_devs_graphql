3a59500031cbcf729efd1d8a4d4fab4b91fc0e69
Israel Borba <israel.borba@totvs.com.br>
2019-11-03 17:29:38 -0300

    Refactor resolver

---
+++ src/resolvers.ts
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch'
+
+const getUsers = () => fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+const getPosts = () => fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
+const getUser = (id: number) => fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(async res => res.json())
+const getPost = (id: number) => fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res => res.json())
+const getPostByUser = (userId: number) => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`).then(res => res.json())
+
+module.exports = {
+  Query: {
+    users: () => getUsers(),
+    posts: (_, __) => getPosts(),
+    user: (_, args) => getUser(args.id),
+    post: (_, args) => getPost(args.id)
+  },
+  Post: {
+    userId: parent => getUser(parent.userId)
+  },
+  User: {
+    posts: parent => getPostByUser(parent.id)
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,12 @@
 import { ApolloServer } from 'apollo-server'
 import { makeExecutableSchema } from 'graphql-tools'
-import fetch from 'node-fetch'
 const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
 const port = 3000
-
-const getUsers = () => fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
-const getPosts = () => fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
-const getUser = (id) => fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(async res => res.json())
-const getPost = (id) => fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res => res.json())
-const getPostByUser = (userId) => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`).then(res => res.json())
 
 const definition = {
   typeDefs,
-  resolvers: {
-    Query: {
-      users: () => getUsers(),
-      posts: (_, __) => getPosts(),
-      user: (_, args) => getUser(args.id),
-      post: (_, args) => getPost(args.id)
-    },
-    Post: {
-      userId: parent => getUser(parent.userId)
-    },
-    User: {
-      posts: parent => getPostByUser(parent.id)
-    }
-  }
+  resolvers
 }
 
 const schema = makeExecutableSchema(definition)
