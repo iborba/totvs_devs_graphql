58faed03d655412ed80df1a1a47cedbb6291158a
Israel Borba <israel.borba@totvs.com.br>
2019-11-03 17:40:09 -0300

    Refactor export

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 const getUsers = () => fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
 const getPosts = () => fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
-const getUser = (id: number) => fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(async res => res.json())
+const getUser = (id: number) => fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res => res.json())
 const getPost = (id: number) => fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res => res.json())
 const getPostByUser = (userId: number) => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`).then(res => res.json())
 
